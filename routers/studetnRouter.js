b59813f00211fc44afe9a8720aba2977782ecb41
Zheltoukhov <dmitwhite@gmai.com>
2022-01-13 21:04:20 +0300

    something

--- routers/studetnRouter.js
+++ routers/studetnRouter.js
@@ -12,6 +12,7 @@ router.get('/register', (req, res) => {
 
 
 router.post('/register', async (req, res) => {
+  console.log(req.body)
   const { studentName, parentName, studentAge, phone, email, password } = req.body;
   const student = await Student.create({ studentName, parentName, studentAge, phone, email, password: sha256(password) })
   req.session.userName = student.name;

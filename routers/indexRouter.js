1ae6ecd804b532d563bc63ad79e58090f8ce91ec
egor <egornick98@gmail.com>
2022-01-13 14:17:20 +0300

    edit

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,9 +1,56 @@
 const router = require('express').Router();
+const sha256 = require('sha256');
 
+
+const { Student, Mentor } = require('../db/models');
 
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/')
+})
+
+
+
+router.get('/signin', (req, res) => {
+  res.render('signin')
+})
+
+
+router.post('/signin', async (req, res) => {
+  
+  const { email, password } = req.body
+  console.log(email)
+  const student = await Student.findOne({ where: { email } });
+  console.log('=====>>>>>>>>>>>>>>>>>>>.',student)
+  const mentor = await Mentor.findOne({ where: { email } });
+try {
+  if (student) {
+    if (student.password === sha256(password)) {
+      req.session.userName = student.name;
+      req.session.userEmail = student.email;
+      req.session.userId = student.id;
+      res.redirect(`/student/${student.id}`)
+    }
+  } else if (mentor) {
+    if (mentor.password === sha256(password)) {
+      req.session.userName = mentor.name;
+      req.session.userEmail = mentor.email;
+      req.session.userId = mentor.id;
+      res.redirect(`/mentor/${mentor.id}`)
+    }
+  } else {
+    res.send('sorry email does not exist')
+  }
+  
+} catch (error) {
+  console.log(error)
+}
+
 })
 
 
---
+++ routers/lessonRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+
+
+
+// router.get('/student/register/lesson', (req, res) => {
+//   res.render('studentLessonRegister')
+// })
+
+// router.post('/student/register/lesson', (req, res) => {
+//   res.redirect(`/user/children/${student.id}`)
+// })
+
+
+
+
+
+
+
+module.exports = router;

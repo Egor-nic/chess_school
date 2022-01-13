1f057c0f796a4c2c889c0b64184a086253bac8b1
egor <egornick98@gmail.com>
2022-01-13 17:12:15 +0300

    pull html

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ const app = express();
 const cookieParser = require('cookie-parser');
 
 const { PORT } = process.env ?? 3011;
+
 const indexRouter = require('./routers/indexRouter');
 const studentRouter = require('./routers/studetnRouter');
 const mentorRouter = require('./routers/mentorRouter');
-const lessonRouter = require('./routers/lessonRouter')
+const lessonRouter = require('./routers/lessonRouter');
+
+// const { checkUser } = require('./middelware/allmiddelware');
 
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
@@ -32,17 +35,12 @@ app.use(session({
   name: 'auth',
 }));
 
-
+// app.use(checkUser);
 
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
-<<<<<<< HEAD
-app.use('/mentor', mentorRouter)
-app.use('/admin', adminRouter)
-=======
 app.use('/mentor', mentorRouter);
 app.use('/lesson', lessonRouter);
->>>>>>> 1cd94390f4d5be6ba5ce6a55e06e5a1f8bf63791
 
 
 
--- routers/studetnRouter.js
+++ routers/studetnRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sha256 = require('sha256');
 
+// const { checkUser } = require('../middelware/allmiddelware');
 const { Student } = require('../db/models');
 
 
@@ -13,20 +14,47 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
   const { studentName, parentName, studentAge, phone, email, password } = req.body;
-  const student = await Student.create({ studentName, parentName, studentAge, phone, email, password: sha256(password) })
-  req.session.userName = student.name;
-  req.session.userEmail = student.email;
-  req.session.userId = student.id;
-  res.redirect(`/student/${student.id}`)
+  const newStudent = await Student.create({ studentName, parentName, studentAge, phone, email, password: sha256(password) })
+  console.log(newStudent)
+  req.session.userName = newStudent.name;
+  req.session.userEmail = newStudent.email;
+  req.session.userId = newStudent.id;
+  const student = {
+    id: 7,
+    studentName: 'test2',
+    parentName: 'mama',
+    studentAge: 21,
+    phone: '123',
+    email: 'test2@test',
+  }
+  res.json(student)
 })
 
 
-router.get('/edit', (req, res) => {
-  res.render('edit')
+
+router.post('/edit/:id', async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id)
+    await Student.update(req.body, { where: { id: req.params.id } });
+    res.redirect(`/student/${student.id}`)
+
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 
 
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id)
+    res.render('edit', { student })
+
+  } catch (error) {
+    console.log(error)
+
+  }
+})
 
 
 router.get('/:id', async (req, res) => {
